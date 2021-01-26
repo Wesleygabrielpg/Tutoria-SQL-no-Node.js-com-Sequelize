8202c1b7c4d5d52b0bf87532dd47c27cd5abb46f
Wesleygabrielpg <wesleygabrielpg2@gmail.com>
2021-01-26 18:30:38 -0300

    commit

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = 
+{
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'PoI148@1590',
+    database: 'sqlnode',
+
+    define:
+    {
+        timestamps: true,
+        underscored: true,
+    }
+}
---
+++ src/database/migrations/20210126211113-create-users.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // O que as migrations precisam fazer se der certo
+    return queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: 
+      {
+        type: Sequelize.STRING,
+        allowNull: false, 
+      },
+      email:
+      {
+        type: Sequelize.STRING,
+        allowNull: false, 
+      },
+      create_at:
+      {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at:
+      {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    // O que as migrations precisam fazer caso algo dê errado
+    // e o que ela precisa desfazer
+    return queryInterface.dropTable('users');
+  }
+};
